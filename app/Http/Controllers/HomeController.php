5b5676aaaae56fb3205f27c1885d3af18674e803
shubham7pathania <shubham.pathania012@gmail.com>
2024-09-17 16:05:12 +0530

    Krde add bhai ab

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,29 +14,4 @@ class HomeController extends Controller
 
         return view('home', compact('users'));
     }
-    public function user_details_show(){
-        return view('details');
-    }
-    public function user_details_store(Request $request){
-
-        $request->validate([
-            'hobbies'=>'required',
-            'phone'=>'required',
-            'gender'=>'required',
-            'company'=>'required',
-        ]);
-
-
-        $userDetails = new UserDetails;
-
-        $userDetails->id = $request->user_id;
-        $userDetails->hobbies = $request->hobbies;
-        $userDetails->phone = $request->phone;
-        $userDetails->gender = $request->gender;
-        $userDetails->company = $request->company;
-
-        $userDetails->save();
-
-        return back()->with('success', 'Your details has been saved');
-    }
 }
